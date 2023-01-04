f809f8fca3fc02b054c475414d063d880e09d3c6
dannybonz <115420954+dannybonz@users.noreply.github.com>
2023-01-04 16:11:38 +0000

    Login system and scoreboard

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ let http = require("http");
 let path = require("path");
 let socketIo = require("socket.io");
 let mongoose = require("mongoose");
+let User = require("./user-schema").User;
 let Pokemon = require("./pokemon-schema").Pokemon;
 let url = "mongodb+srv://adminn:alienalien@cluster0.my31ewo.mongodb.net/pokemondb?retryWrites=true&w=majority";
 let port = 9000;
 let pokemon = null;
 let correct_answer = null;
 let all_pokemon = [];
+let sessions = {};
+let leaderboard = null;
 
 // Initialise the app and server.
 let app = express();
 let server = http.createServer(app);
+
+// Setup the websocket.
+let io = socketIo(server);
 
 // Connect to the Mongo database using Mongoose.
 mongoose.connect(url, {useUnifiedTopology: true, useNewUrlParser: true});
@@ -51,27 +57,70 @@ function refresh_pokemon() {
 	correct_answer = chosen_pokemon["name"]
 }
 
+function update_leaderboard() {
+	User.find({user_score:{$gt: 1}}, 'user_name user_score').sort({user_score : "desc"}).exec(function(err, output) {
+		leaderboard = output;
+		io.emit("leaderboard", leaderboard) //Send the new leaderboard to everyone
+	});
+}
+
+update_leaderboard();
+
 //Testing feature
 app.get("/hello", function(request, response) {
 	response.send("Hello World");
 });
 
-// Setup the websocket.
-let io = socketIo(server);
-
 io.on("connection", function(socket) {
-	//Emit the current question to newly connected users
-	socket.emit("new pokemon", pokemon);
-	console.log("New user connection");
+	console.log("New connection");
+	
+	socket.on("joined game", function(session_var) {
+		if (sessions[session_var]==undefined) {
+			socket.emit("not logged in");
+		} else {
+			socket.emit("welcome", sessions[session_var]); //Login confirmed, activate input		
+		}
+		socket.emit("new pokemon", pokemon); //Send the current question
+		socket.emit("leaderboard", leaderboard); //Send the current leaderboard
+	})
+	
+	socket.on("registration", function(user_data) {
+		User.findOne({ user_name : user_data[0] }).exec().then((doc) => {
+			if (doc) { //A user with this name already exists
+				socket.emit("username used");
+			} else { //No user with this name exists yet
+				User.create({ user_name : user_data[0], user_password : user_data[1], user_score : 0 });
+				socket.emit("registration successful");
+			}
+		})
+	});
+
+	socket.on("login", function(user_data) {
+		User.findOne({ user_name : user_data[0], user_password : user_data[1] }).exec().then((doc) => {
+			if (doc) {
+				session_var = Math.random().toString(16).slice(2);
+				sessions[session_var]=user_data[0];
+				socket.emit("login successful",session_var);
+				console.log("logged in");
+			} else {
+				socket.emit("login failed");
+			}
+		})
+	});
 
 	socket.on("sent message", function(msg) {
 		// Send message to all other users
 		console.log("Message received");
-		io.emit("received message", msg);
-		if (msg[0]==correct_answer) {
-			io.emit("correct", correct_answer);
-			refresh_pokemon();
-			io.emit("new pokemon", pokemon);
+		io.emit("received message", [msg[0], sessions[msg[1]]]);
+		if (msg[0].toLowerCase()==correct_answer.toLowerCase()) {
+			User.findOne({user_name : sessions[msg[1]]}).exec().then((doc) => {
+				User.updateOne({ user_name: sessions[msg[1]] }, { user_score: doc.user_score+1 }, function() {
+					io.emit("correct", [correct_answer,sessions[msg[1]],doc.user_score+1]);
+					refresh_pokemon();
+					io.emit("new pokemon", pokemon);
+					update_leaderboard();
+				});
+			});
 		}
 	});
 });
---
+++ user-schema.js
@@ -0,0 +1,11 @@
+let mongoose = require("mongoose");
+
+let user_schema = new mongoose.Schema({
+	user_name: String,
+	user_password: String,
+	user_score: Number
+}, {collection: "users"});
+
+let User = mongoose.model("user", user_schema);
+
+module.exports.User = User;
