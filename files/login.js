9433ea1b0cbbac64b9517504fc54104021ab58c5
dannybonz <115420954+dannybonz@users.noreply.github.com>
2023-01-05 16:04:32 +0000

    Additional comments and other tweaks

--- files/login.js
+++ files/login.js
@@ -1,5 +1,5 @@
 $(function() {
-
+	//Connect socket
 	let socket = io("http://localhost:9000");
 
 	$("#register").click(function() {
@@ -9,31 +9,36 @@ $(function() {
 	$("#login").click(function() {
 		socket.emit("login", [$("#login_name").val(),$("#login_password").val()]);
 	})
-		
+
+	//Received after attempting to register an account with a username that already exists
 	socket.on("username used", function() {
 		$("#register-error").html("This username is already in use.");
 	});
 
+	//Received after successfully creating an account
 	socket.on("registration successful", function() {
-		$("#register-error").html("success!");
+		$("#register-error").html("Your account has now been succesfully created! Log in to continue.");
 	});
 
+	//Received after attempting to log into an account with incorrect credentials
 	socket.on("login failed", function() {
 		$("#login-error").html("That username or password combination doesn't exist.");
 	});
 
+	//Received after attempting to register an account with a username that is too short
 	socket.on("username too short", function() {
 		$("#register-error").html("The provided username is too short.");
 	});
 
+	//Received after attempting to register an account with a password that is too short
 	socket.on("password too short", function() {
 		$("#register-error").html("The provided password is too short.");
 	});
 
+	//Received after succesfully logging in
 	socket.on("login successful", function(session) {
-		document.cookie = session;
-		window.location.replace("game.html");
+		document.cookie = session; //Store provided session value in a cookie
+		window.location.replace("game.html"); //Redirect to game page
 	});
-
 });
 
--- files/pokemon.js
+++ files/pokemon.js
@@ -1,100 +1,115 @@
-function update_score_display(user_data) {
-	$("#signed_in").html('Logged in as '+user_data[0].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;')+' ('+user_data[1]+' points)<button class="game_login" id="sign_out">Sign Out</button>');
-}
-
 $(function() {
-		
+	//Connect socket
 	let socket = io("http://localhost:9000");
-	let deadline = null;
+	let deadline = null; //Used to store remaining time on question
 
-	socket.on("not logged in", function() {
+	//Used to send a guess to the server
+	function submit_guess() {
+		socket.emit("sent guess", [$("#guess_entry").val(),document.cookie]); //Send guess along with session cookie
+		$("#guess_entry").val(""); //Clear entry
+	}
+	
+	//Used to update the score and "logged in as" message
+	function update_score_display(user_data) {
+		$("#signed_in").html('Logged in as '+user_data[0].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;')+' ('+user_data[1]+' points)<button class="game_login" id="sign_out">Sign Out</button>');
+	}	
+
+	//This message is sent by the server if the client is not logged in
+	socket.on("not logged in", function() { 
 		$(".not_logged_in").css("display", "block");
 	});
 	
+	//This message is sent by the server if the client is logged in
 	socket.on("welcome", function(user_data) {
-		$("#guess_entry").attr("disabled",false);
-		$("#send").attr("disabled",false);
-		$("#send").css("visibility","visible");
-		$("#input_section").css("display","flex");
-		$("#guess_entry").attr("value","");
-		$("#input_section").css("background-color","#FFFFFF");
-		$("#signed_out").css("display","none");
-		$("#signed_in").css("display","block");		
+		$("#guess_entry").attr("disabled",false); //Enable guess entry input
+		$("#send").attr("disabled",false); //Enable send button
+		$("#send").css("visibility","visible"); //Make the send button visible
+		$("#guess_entry").attr("value",""); //Reset the entry text
+		$("#input_section").css("background-color","#FFFFFF"); //Make guess entry background colour white
+		$("#signed_out").css("display","none"); //Hide the "signed out" message
+		$("#signed_in").css("display","block"); //Show the "signed in" message
 		update_score_display(user_data);
 	});
 	
+	//This message is sent by the server when another user submits a guess
 	socket.on("received message", function(msg) {
-		$("#guess_box").append('<p class="message"><b>'+msg[1].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;') + ':</b> '+msg[0].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;') + '</p>');
-		$("#guess_box").animate({ scrollTop: $('#guess_box').prop("scrollHeight")}, 500);		
+		$("#guess_box").append('<p class="message"><b>'+msg[1].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;') + ':</b> '+msg[0].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;') + '</p>'); //Display the other user's guess, escaping special characters
+		$("#guess_box").animate({scrollTop: $('#guess_box').prop("scrollHeight")}, 500); //Smoothly animate the guess box scrolling the new message into view
 	});
 
+	//This message is sent by the sever when nobody answers the question in time
 	socket.on("failed", function(answer) {
-		$("#guess_box").append('<p class="server_message">Nobody got it? The answer was '+answer+'!</p>');
-		$("#guess_box").animate({ scrollTop: $('#guess_box').prop("scrollHeight")}, 500);
+		$("#guess_box").append('<p class="server_message">Nobody got it? The answer was '+answer+'!</p>'); //Display the correct answer in the guesses box
+		$("#guess_box").animate({scrollTop: $('#guess_box').prop("scrollHeight")}, 500);
 	});
-
+	
+	//This message is sent by the server when the leaderboard updates
 	socket.on("leaderboard", function(leaderboard) {
 		let full_string="";
-		let pos=1;
-		leaderboard.forEach(function (item, index) {
-		  full_string=full_string.concat("<p class='leaderboard_position'><b>#"+pos+" "+item["user_name"].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;')+":</b> "+item["user_score"]+"<br></p>");
+		let pos=1; //Used for display of #1, #2, #3, etc.
+		leaderboard.forEach(function (item, index) { //Loop through every leaderboard entry
+		  full_string=full_string.concat("<p class='leaderboard_position'><b>#"+pos+" "+item["user_name"].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;')+":</b> "+item["user_score"]+"<br></p>"); //Add the user and score into the leaderboard HTML string, escaping special characters
 		  pos+=1;
 		});
-		console.log(full_string);
-		$("#leaderboard_contents").html(full_string);
+		$("#leaderboard_contents").html(full_string); //Display the finished leaderboard contents
 	});
 
+	//This message is sent by the server when a user guesses correctly
 	socket.on("correct", function(answer) {
-		$("#guess_box").append("<p class='correct'>It's "+answer[0]+"!<br>"+answer[1]+" now has "+answer[2]+" points.</p>");
-		$("#guess_box").animate({ scrollTop: $('#guess_box').prop("scrollHeight")}, 500);
+		$("#guess_box").append("<p class='correct'>It's "+answer[0]+"!<br>"+answer[1].replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;')+" now has "+answer[2]+" points.</p>"); //Display the correct answer, along with the username of the person who guessed it, escaping special characters
+		$("#guess_box").animate({scrollTop: $('#guess_box').prop("scrollHeight")}, 500);
 	});
 
+	//This message is sent by the server when the client's score has changed
 	socket.on("update score", function(user_data) {
 		update_score_display(user_data);
 	});
 
-
+	//This message is sent by the server when a new question has been generated
 	socket.on("new pokemon", function(pokemon) {
-		$("#guess_box").append('<p class="server_message">A new Pokémon appeared!</p>');
+		$("#guess_box").append('<p class="server_message">A new Pokémon appeared!</p>'); 
 		$("#guess_box").animate({ scrollTop: $('#guess_box').prop("scrollHeight")}, 500);		
-		$("#pokemon_image").css('background-image', 'url(' + pokemon[0] + ')');
+		$("#pokemon_image").css('background-image', 'url('+pokemon[0]+')'); //Set new Pokémon image
 		deadline = pokemon[1];
 		let now = new Date();
-		let time_since_epoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
+		let time_since_epoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000); //Find time since epoch (negates timezone differences)
 		let difference = deadline - time_since_epoch;
-		remaining_time = difference;
+		remaining_time = difference; //Time until deadline passes
 	});
 	
+	//This message is sent by the server if the client attempst to submit a guess when it is not logged in
 	socket.on("login required", function() {
-		window.location.replace("login.html");
+		window.location.replace("login.html"); //Redirect to login page 
 	});
 
+	//When the Send button is clicked
 	$("#send").click(function() {
-		socket.emit("sent message", [$("#guess_entry").val(),document.cookie]);
-		$("#guess_entry").val("");
+		submit_guess();
 	});
 	
-	$("#sign_out").click(function() {
-		socket.emit("log out", document.cookie);
-		document.cookie="";
-		location.reload();		
-	});
-
+	//If a key is pressed within the input box
 	$('#guess_entry').keypress(function (e) {
-	  if (e.which == 13) {
-		socket.emit("sent message", [$("#guess_entry").val(),document.cookie]);
-		$("#guess_entry").val("");
+	  if (e.which == 13) { //If the Enter key is pressed
+		submit_guess(); 
 	  }
 	});
 
-	socket.emit("joined game", document.cookie);
+	//Event listener for clicking on sign out button
+	$("body").on("click", "#sign_out", function(){
+		socket.emit("log out", document.cookie); //Tell server this client is logging out
+		document.cookie=""; //Reset cookie
+		location.reload(); //Refresh page
+	});
+
+	socket.emit("joined game", document.cookie); //Tell server this client has joined the game
  
-	var intervalId = window.setInterval(function(){
+	//Set interval for updating the current question's remaining time
+	var intervalId = window.setInterval(function(){ 
 		let now = new Date();
 		let time_since_epoch = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
 		let difference = deadline - time_since_epoch;
 		remaining_time = difference;
-		$("#timer").width(Math.floor(remaining_time/1000)+"%");
+		$("#timer").width(Math.floor(remaining_time/1000)+"%"); //Set width of timer to reflect seconds remaining
 	}, 1000);
 });
 
